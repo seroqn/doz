05398b2c4072808cd49e72d0b0c6522f8528109c
seroqn <sero.qn@gmail.com>
2022-06-30 17:06:34 +0900

    mv kinds/ -> shdokind/

--- t/kinds/hint_test.ts
+++ t/kinds/hint_test.ts
@@ -1,33 +1,33 @@
-import {_parseHint} from "kinds/hint/hint.ts";
-import { assertEquals, afterEach, describe, it } from "t/deps.ts"
+import { _parseHint } from "kind/hint/hint.ts";
+import { assertEquals, describe, it } from "t/deps.ts";
 
-describe('kinds/hint', ()=>{
-  describe('_parseHint()', ()=>{
-    it('skip empty string', ()=>{
-      assertEquals(_parseHint(''), null)
-      assertEquals(_parseHint(' '), null)
-      assertEquals(_parseHint('  '), null)
-    })
-    it('skip empty label', ()=>{
-      assertEquals(_parseHint('::'), null)
-      assertEquals(_parseHint(' ::'), null)
-      assertEquals(_parseHint('::foo'), null)
-      assertEquals(_parseHint(' :: foo'), null)
-    })
-    it('return only label when description is empty', ()=>{
-      assertEquals(_parseHint('foo'), 'foo\t')
-      assertEquals(_parseHint(' foo  '), 'foo\t')
-      assertEquals(_parseHint('foo ::'), 'foo\t')
-      assertEquals(_parseHint(' foo ::'), 'foo\t')
-      assertEquals(_parseHint('foo :: '), 'foo\t')
-    })
-    it('return rabel and description when both is given', ()=>{
-      assertEquals(_parseHint('foo ::FOO'), 'foo\tFOO')
-      assertEquals(_parseHint(' foo  ::  FOO'), 'foo\tFOO')
-      assertEquals(_parseHint('foo bar ::FOO  BAR'), 'foo bar\tFOO  BAR')
-    })
-    it('beging `#`, regard it as comment', ()=>{
-      assertEquals(_parseHint('# foo bar  baz'), '\t- foo bar  baz')
-    })
-  })
-})
+describe("kinds/hint", () => {
+  describe("_parseHint()", () => {
+    it("skip empty string", () => {
+      assertEquals(_parseHint(""), null);
+      assertEquals(_parseHint(" "), null);
+      assertEquals(_parseHint("  "), null);
+    });
+    it("skip empty label", () => {
+      assertEquals(_parseHint("::"), null);
+      assertEquals(_parseHint(" ::"), null);
+      assertEquals(_parseHint("::foo"), null);
+      assertEquals(_parseHint(" :: foo"), null);
+    });
+    it("return only label when description is empty", () => {
+      assertEquals(_parseHint("foo"), "foo\t");
+      assertEquals(_parseHint(" foo  "), "foo\t");
+      assertEquals(_parseHint("foo ::"), "foo\t");
+      assertEquals(_parseHint(" foo ::"), "foo\t");
+      assertEquals(_parseHint("foo :: "), "foo\t");
+    });
+    it("return rabel and description when both is given", () => {
+      assertEquals(_parseHint("foo ::FOO"), "foo\tFOO");
+      assertEquals(_parseHint(" foo  ::  FOO"), "foo\tFOO");
+      assertEquals(_parseHint("foo bar ::FOO  BAR"), "foo bar\tFOO  BAR");
+    });
+    it("beging `#`, regard it as comment", () => {
+      assertEquals(_parseHint("# foo bar  baz"), "\t# foo bar  baz");
+    });
+  });
+});
