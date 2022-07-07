a74f68df0e51be20f99924180e8649c22489bd93
seroqn <sero.qn@gmail.com>
2022-07-08 08:36:12 +0900

    kind/hint;; change printing style: (use WIDTH_THRESHOLD)

--- dozkind/hint/hint.ts
+++ dozkind/hint/hint.ts
@@ -1,4 +1,8 @@
+import { sprintf } from "https://deno.land/std@0.144.0/fmt/printf.ts";
+
+const WIDTH_THRESHOLD = 12;
 type Hints = Array<string | undefined>;
+type HPair = [string, string];
 function isHints(hints: any): hints is Hints {
   return (
     Array.isArray(hints) &&
@@ -7,7 +11,6 @@ function isHints(hints: any): hints is Hints {
 }
 
 export function fire(_: string, what: any) {
-  let outs: string[] = [];
   if (typeof what != "object") {
     console.error(`invalid "what": ${JSON.stringify(what)}`);
     Deno.exit(1);
@@ -16,23 +19,40 @@ export function fire(_: string, what: any) {
     console.error(`invalid "hints": ${JSON.stringify(what.hints)}`);
     Deno.exit(1);
   }
-  for (let hint of what.hints) {
-    let out = _parseHint(hint);
-    if (out) {
-      outs.push(out);
-    }
-  }
+
+  const fmt = `%-${WIDTH_THRESHOLD}s %s  %s`;
+  const hashIndent = " ".repeat(WIDTH_THRESHOLD + 2);
+  const outs = what.hints.map((hint: string | null) => _hintIntoHPair(hint))
+    .filter((pair: HPair | null) => pair).map((pair: HPair) => {
+      if (!pair[0]) {
+        return hashIndent + pair[1];
+      }
+      const sep = pair[1] ? "::" : "";
+      return _isLong(pair[0])
+        ? `${pair[0]}\t${sep}  ${pair[1]}`
+        : sprintf(fmt, pair[0], sep, pair[1]);
+    });
   return [true, outs.join("\n")];
 }
-export function _parseHint(hint: string | null) {
+export function _hintIntoHPair(hint: string | null): HPair {
   if (!hint) {
     return null;
   } else if (/^#/.test(hint)) {
-    return hint.replace(/^(#+)\s?/, "\t$1 ");
+    return [
+      "",
+      hint.replace(
+        /^(#)(#*)\s?/,
+        (_: string, p1: string, p2: string) => `${p1}  ${p2}${p2 ? " " : ""}`,
+      ),
+    ];
   }
   const result = /^\s*((?::(?::\S|[^:])|[^: ]).*?)(?:\s*|\s*::(?:\s*|\s+(.*)))$/
     .exec(hint); // `:: ` is separator
-  return result
-    ? `${result[1].trim()}${result[2] ? "\t" : ""}${result[2] ?? ""}`
-    : null;
+  return !result ? null : [result[1].trim(), result[2] ?? ""];
+}
+function _isLong(str: string) {
+  return [...str].reduce(
+    (count: number, char: string) => Math.min(new Blob([char]).size, 2) + count,
+    0,
+  ) > WIDTH_THRESHOLD;
 }
